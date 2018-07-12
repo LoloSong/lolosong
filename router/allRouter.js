97048d579557ecaceca19862c08597aef493ac78
LOLO <279144728@qq.com>
2018-07-12 10:51:21 +0800

    update

--- router/allRouter.js
+++ router/allRouter.js
@@ -5,15 +5,21 @@ const blog = require('./blog')
 const blogAdmin = require('./blogAdmin')
 const page1 = require('./page1')
 
-// 博客
-router.get('/', async (ctx) => {
-  let data = await util.readFile('./views/blog/index.html')
+// admin
+router.get('/admin/*', async (ctx) => {
+  let data = await util.readFile('./views/admin/index.html')
   ctx.body = data
 })
-router.use('/api/blog', blog.routes())
 router.use('/api/admin', blogAdmin.routes())
 
 // 微信
 router.use('/wechat/page1', page1.routes())
+
+// blog(放在最后)
+router.get('/*', async (ctx) => {
+  let data = await util.readFile('./views/blog/index.html')
+  ctx.body = data
+})
+router.use('/api/blog', blog.routes())
 
 module.exports = router
