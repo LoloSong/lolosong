70278995c1fe42352fc142f315d991e1e42860fd
LOLO <279144728@qq.com>
2018-07-12 14:10:01 +0800

    seo:title,desc,keyword

--- router/allRouter.js
+++ router/allRouter.js
@@ -5,15 +5,20 @@ const blog = require('./blog')
 const blogAdmin = require('./blogAdmin')
 const page1 = require('./page1')
 
-// 博客
-router.get('/', async (ctx) => {
-  let data = await util.readFile('./views/blog/index.html')
-  ctx.body = data
-})
-router.use('/api/blog', blog.routes())
-router.use('/api/admin', blogAdmin.routes())
-
 // 微信
 router.use('/wechat/page1', page1.routes())
 
+// 博客后台
+router.use('/api/admin', blogAdmin.routes())
+router.get('/admin/*', async (ctx) => {
+  let data = await util.readFile('./views/admin/index.html')
+  ctx.body = data
+})
+
+// 博客
+router.use('/api/blog', blog.routes())
+router.get('/*', async (ctx) => {
+  let data = await util.readFile('./views/blog/index.html')
+  ctx.body = data
+})
 module.exports = router
